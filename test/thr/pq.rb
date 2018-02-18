9f602254d151c5ba25bee1999d94cb6a399b2ba4
jeppeter <jeppeter@gmail.com>
2018-02-18 21:13:21 +0800

    to add pq.rb

--- test/thr/pq.rb
+++ test/thr/pq.rb
@@ -11,14 +11,33 @@ class LogClone
 		@logfmt = logfmt
 		@thr = nil
 		@exited = true
+		@exitflag = false
+		@mutex = nil
 		@exitcode = 0
 		@queue = nil
+	end
+
+	def insert_queue(obj)
+		@mutex.synchronize {
+			@queue.push(obj)
+		}
 	end
 
 	def start_run()
 		if ! @exited 
 			return -1
 		end
-		
+		__inner_free = lambda {   
+			if @thr != nil && ! @exited {
+				@exitflag = true
+				while ! @exited {
+					sleep 0.1
+					insert_queue(nil)
+				}
+			}
+		} 
+
+
+
 	end
 end
